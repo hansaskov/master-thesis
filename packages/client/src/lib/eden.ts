73a98dff01dd53c2ffee351f11dd400928782d47
Hans Askov <hans@askov.dk>
2024-09-01 22:07:42 +0200

    Update build methods

--- packages/client/src/lib/eden.ts
+++ packages/client/src/lib/eden.ts
@@ -1,6 +1,8 @@
 import { PUBLIC_SERVER_URL } from '$env/static/public';
 import { treaty } from '@elysiajs/eden'
 import type { App } from "backend";
+import { dev } from "$app/environment"
 
+const serverURL = dev ? "localhost:1420" : "localhost:3000"
 
-export const api = treaty<App>(PUBLIC_SERVER_URL).api
+export const api = treaty<App>(serverURL).api
