eb238404c0684a67b715bbd908620ca270d9d12c
Hans Askov <hansaskov1999@gmail.com>
2024-09-03 23:06:20 +0200

    cargo clippy

--- packages/ingestion/src/client.rs
+++ packages/ingestion/src/client.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{Result};
 use reading::{conditions_service_client::ConditionsServiceClient, ConditionsRequest, Reading};
 use std::time::Duration;
 use tokio::{signal, sync::mpsc, time::interval};
