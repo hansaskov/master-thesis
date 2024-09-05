1dd39fd33446c69b9610c52bbcf9c71418968c42
Hans Askov <ha@trivision.dk>
2024-09-05 16:18:06 +0200

    Remove tauri vite code

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -14,20 +14,6 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
     ...(isDev && {
       proxy: {
         '/api': 'http://localhost:3000'
