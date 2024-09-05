964d3f43ec0f51efca59e0dc01fba9c2d2008ab1
Hans Askov <ha@trivision.dk>
2024-09-05 16:18:13 +0200

    Fix bug with eden

--- packages/frontend/src/lib/eden.ts
+++ packages/frontend/src/lib/eden.ts
@@ -2,6 +2,6 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from "backend";
 import { dev } from "$app/environment"
 
-const serverURL = dev ? "localhost:1420" : "localhost:3000"
+const serverURL = dev ? "localhost:5173" : "localhost:3000"
 
 export const api = treaty<App>(serverURL).api
