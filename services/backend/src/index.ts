5523875a9452c26a2b019258ace898f2bf2bc554
hansaskov <hansaskov1999@gmail.com>
2024-09-14 10:10:14 +0200

    Update readme

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -2,19 +2,22 @@ import { swagger } from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
 
 const api = new Elysia()
-	.onBeforeHandle(({request}) => { console.log(request.url)})
-	.post("/hello", ({body}) => `Hello, ${body.name}! You've been greeted from The server!`, {
-		body: t.Object({
-			name: t.String()
-		})
-	})
+  .onBeforeHandle(({ request }) => console.log(request.url))
+  .post(
+    "/hello",
+    ({ body }) => `Hello, ${body.name}! You've been greeted from The server!`,
+    {
+      body: t.Object({
+        name: t.String(),
+      }),
+    },
+  );
 
+const app = new Elysia({ prefix: "/api" })
+  .use(swagger())
+  .use(api)
+  .listen(process.env.PORT as string);
 
-const app = new Elysia({prefix: "/api"})
-	.use(swagger())
-	.use(api)
-	.listen(process.env.PORT as string);
+console.log(`🦊 Server started at ${app.server?.url.origin}`);
 
-console.log(`🦊 Server started at ${app.server?.url.origin}`)
-	
 export type App = typeof app;
