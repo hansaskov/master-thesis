58890e469b1e475ee44114990796562759148076
Hans Askov <hansaskov1999@gmail.com>
2024-09-18 08:09:27 +0200

    Move login page to the root

---
+++ services/frontend/src/routes/(site)/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+  </script>
+  
+  <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div class="flex items-center justify-center py-12">
+      <div class="mx-auto grid w-[350px] gap-6">
+        <div class="grid gap-2 text-center">
+          <h1 class="text-3xl font-bold">Welcome Back</h1>
+          <p class="text-balance text-muted-foreground">
+            Log in to your account using your Microsoft credentials
+          </p>
+        </div>
+        <div class="grid gap-4">
+          <Button href="/systems" variant="outline" class="w-full">
+            Login with Microsoft
+          </Button>
+        </div>
+        <div class="mt-4 text-center text-sm">
+          Don't have an account?
+          <a href="/signup" class="underline"> Contact your administrator </a>
+        </div>
+      </div>
+    </div>
+    <div class="bg-muted hidden lg:block">
+      <img
+        src="/placeholder.svg"
+        alt="TriVision Quality Control System"
+        width="1920"
+        height="1080"
+        class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+      />
+    </div>
+  </div>
