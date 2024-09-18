7a288f9433a46be588fd1cabbdddf75d205ad4fe
Hans Askov <hansaskov1999@gmail.com>
2024-09-18 11:59:37 +0200

    Refactor combobox into it's own component

--- services/frontend/src/routes/(app)/systems/+page.svelte
+++ services/frontend/src/routes/(app)/systems/+page.svelte
@@ -3,15 +3,13 @@
 	import { Badge } from '$lib/components/ui/badge';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import * as Table from '$lib/components/ui/table';
-	import * as Command from '$lib/components/ui/command';
-	import * as Popover from '$lib/components/ui/popover';
+
 	import * as Tabs from '$lib/components/ui/tabs';
 	import { goto } from '$app/navigation';
-	import { Check } from 'svelte-radix';
-	import { cn } from '$lib/utils';
+
 	import { tick } from 'svelte';
+	import OrgCombobox from './OrgCombobox.svelte';
 
 	const statusTypes = ['Active', 'Offline', 'Paused'] as const;
 	type StatusType = (typeof statusTypes)[number];
@@ -135,25 +133,6 @@
 		goto(`/systems/${id}`);
 	}
 
-	const organizations = [
-		{ value: 'org1', label: 'Organization 1' },
-		{ value: 'org2', label: 'Organization 2' },
-		{ value: 'org3', label: 'Organization 3' },
-		{ value: 'org4', label: 'Organization 4' }
-	];
-
-	let openCombobox = false;
-	let selectedOrg = '';
-
-	$: selectedOrgLabel =
-		organizations.find((org) => org.value === selectedOrg)?.label ?? 'Select an organization...';
-
-	function closeAndFocusTrigger(triggerId: string) {
-		openCombobox = false;
-		tick().then(() => {
-			document.getElementById(triggerId)?.focus();
-		});
-	}
 </script>
 
 <Tabs.Root bind:value={activeTab}>
@@ -172,42 +151,8 @@
 					Monitor and manage your production systems across different factory areas.
 				</Card.Description>
 			</div>
-			<Popover.Root bind:open={openCombobox} let:ids>
-				<Popover.Trigger asChild let:builder>
-					<Button
-						builders={[builder]}
-						variant="outline"
-						role="combobox"
-						aria-expanded={openCombobox}
-						class="w-[200px] justify-between"
-					>
-						{selectedOrgLabel}
-						<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-					</Button>
-				</Popover.Trigger>
-				<Popover.Content class="w-[200px] p-0">
-					<Command.Root>
-						<Command.Input placeholder="Search organization..." />
-						<Command.Empty>No organization found.</Command.Empty>
-						<Command.Group>
-							{#each organizations as org}
-								<Command.Item
-									value={org.value}
-									onSelect={(currentValue) => {
-										selectedOrg = currentValue;
-										closeAndFocusTrigger(ids.trigger);
-									}}
-								>
-									<Check
-										class={cn('mr-2 h-4 w-4', selectedOrg !== org.value && 'text-transparent')}
-									/>
-									{org.label}
-								</Command.Item>
-							{/each}
-						</Command.Group>
-					</Command.Root>
-				</Popover.Content>
-			</Popover.Root>
+			<OrgCombobox></OrgCombobox>
+			
 		</Card.Header>
 		<Card.Content>
 			<Tabs.Content value={activeTab}>
