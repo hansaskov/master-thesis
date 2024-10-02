01a895ab08aff628d6fae29dacf3ea41ac17b7f6
Hans Askov <hansaskov1999@gmail.com>
2024-10-02 13:49:03 +0200

    Update nixos

--- ci-cd/nixos/remote-update-ssh.ps1
+++ ci-cd/nixos/remote-update-ssh.ps1
@@ -8,10 +8,7 @@ $DEST_IP = "188.245.160.211"
 # Set the destination directory
 $DEST_DIR = "/etc/nixos"
 
-$FILE = "configuration.nix"
+$FILE_1 = "ssh-keys.nix"
 
 # Use scp to copy the files to the remote server
-& scp "$SOURCE_DIR\$FILE" "${DEST_USER}@${DEST_IP}:$DEST_DIR/$FILE"
-
-# SSH into the server and run nixos-rebuild switch
-& ssh "${DEST_USER}@${DEST_IP}" "nixos-rebuild switch"
+& scp "$SOURCE_DIR\$FILE_1" "${DEST_USER}@${DEST_IP}:$DEST_DIR/$FILE_1"
