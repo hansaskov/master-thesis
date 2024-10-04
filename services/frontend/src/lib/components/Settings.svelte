8625dfc9f74efd85cac4cf78fdf9b4fc801be9a0
Hans Askov <hansaskov1999@gmail.com>
2024-10-04 13:32:37 +0200

    Add settings page

--- services/frontend/src/lib/components/Settings.svelte
+++ services/frontend/src/lib/components/Settings.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
     import { Button } from '$lib/components/ui/button';
     import Settings from 'lucide-svelte/icons/settings';
+
+	import { page } from '$app/stores'
+
+    let pathname = $page.url.pathname
+    $: lastPathname = $page.url.pathname.split('/').filter(Boolean).at(-1)
+
+    let blacklist = ["settings"]
+
 </script>
 
-<Button variant="outline" size="icon" class="ml-auto h-9 w-9 bg-background">
-	<Settings class="h-4 w-4" />
-	<span class="sr-only">Settings</span>
-</Button>
-    
+{#if lastPathname && !blacklist.includes(lastPathname)}
+    <Button href={`${pathname}/settings`} variant="outline" size="icon" class="ml-auto h-9 w-9 bg-background">
+        <Settings class="h-4 w-4" />
+        <span class="sr-only">Settings</span>
+    </Button>
+{/if}
