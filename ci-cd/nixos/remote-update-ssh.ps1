e104140235ef1c11df82f79e14cb0dfaf5327d1f
Hans Askov <hansaskov1999@gmail.com>
2024-10-09 10:31:40 +0200

    Update readme and script

--- ci-cd/nixos/remote-update-ssh.ps1
+++ ci-cd/nixos/remote-update-ssh.ps1
@@ -14,3 +14,6 @@ $FILE_2 = "ssh-keys.nix"
 # Use scp to copy the files to the remote server
 & scp "$SOURCE_DIR\$FILE_1" "${DEST_USER}@${DEST_IP}:$DEST_DIR/$FILE_1"
 & scp "$SOURCE_DIR\$FILE_2" "${DEST_USER}@${DEST_IP}:$DEST_DIR/$FILE_2"
+
+# Will set the permissions such that only the root user can read and write the file
+& ssh "${DEST_USER}@${DEST_IP}" "chmod 600 $DEST_DIR/$FILE_2"
