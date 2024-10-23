b4b1942d3fba8d3219268830c710c8b92ee7d947
Hans Askov <hans@askov.dk>
2024-10-23 23:00:44 +0200

    Fix back button to properly use the window.history api

--- services/frontend/src/routes/(app)/systems/[id]/+layout.svelte
+++ services/frontend/src/routes/(app)/systems/[id]/+layout.svelte
@@ -3,20 +3,31 @@
 	import { page } from '$app/stores';
 	import { Button } from '$lib/components/ui/button';
 
-	$: lowerPathname = $page.url.pathname.split('/').filter(Boolean);
-	$: backUrl = '/' + lowerPathname.slice(0, -1).join('/');
-	$: previousSection = lowerPathname.at(-2) || '';
+	let canGoBack = false;
+	$: previousSection = $page.url.pathname.split('/').pop();
+
+	$: {
+		canGoBack = window.history.length > 1;
+	}
+
+	function handleGoBack() {
+    if (canGoBack) {
+      history.back();
+    } else {
+      // For playground testing - just log when we can't go back
+      console.log("No history available, would redirect to home");
+    }
+  }
+
 </script>
 
-{#if lowerPathname.length > 1}
-	
-	<Button href={backUrl} variant="ghost" class="inline-flex md:hidden -ml-4">
+
+	<Button on:click={handleGoBack} variant="ghost" class="inline-flex md:hidden -ml-2">
 		<ArrowLeft class="mr-2 h-4 w-4" />
 		Back to
 		<span class="ml-1 capitalize">{previousSection}</span>
 	</Button>
 	
-{/if}
 <div class="py-4 md:container">
 	<slot />
 </div>
