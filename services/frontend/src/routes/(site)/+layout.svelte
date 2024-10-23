b114b44048c9e85da7916aae2d32233dfa13075e
Hans Askov <hans@askov.dk>
2024-10-23 23:08:59 +0200

    Fix vertical scroll issue on the front page

--- services/frontend/src/routes/(site)/+layout.svelte
+++ services/frontend/src/routes/(site)/+layout.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
-	import Package2 from 'lucide-svelte/icons/package-2';
-	import { Button } from '$lib/components/ui/button';
+    import Package2 from 'lucide-svelte/icons/package-2';
+    import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex min-h-screen w-full flex-col bg-background/40 text-foreground">
-	<header
-		class="sticky top-0 z-30 flex h-14 items-center justify-between border-b bg-background px-4 sm:px-6"
-	>
-		<div class="flex items-center">
-			<a href="/" class="flex items-center gap-2 text-lg font-semibold">
-				<Package2 class="h-6 w-6" />
-				<span>TriVision</span>
-			</a>
-		</div>
-		<nav>
-			<Button href="/" variant="outline">Login</Button>
-		</nav>
-	</header>
-
-	<main class="flex-1 bg-background p-4 sm:p-6">
-		<slot></slot>
-	</main>
-
-	<footer class="border-t bg-background p-4 text-center text-sm text-muted-foreground">
-		© 2024 TriVision. All rights reserved.
-	</footer>
+<div class="flex h-screen w-full flex-col bg-background/40 text-foreground">
+    <header class="flex h-14 items-center justify-between border-b bg-background px-4 sm:px-6">
+        <div class="flex items-center">
+            <a href="/" class="flex items-center gap-2 text-lg font-semibold">
+                <Package2 class="h-6 w-6" />
+                <span>TriVision</span>
+            </a>
+        </div>
+        <nav>
+            <Button href="/" variant="outline">Login</Button>
+        </nav>
+    </header>
+    
+    <main class="flex-1 bg-background">
+        <slot></slot>
+    </main>
+    
+    <footer class="border-t bg-background p-4 text-center text-sm text-muted-foreground">
+        © 2024 TriVision. All rights reserved.
+    </footer>
 </div>
