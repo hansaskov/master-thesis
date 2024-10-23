6c6b2f40614af7d4df03242e2ed2b4346ca3e41e
Hans Askov <hansaskov1999@gmail.com>
2024-10-23 12:53:28 +0200

    Update nixos script and readme

--- ci-cd/nixos/remote-update.ps1
+++ ci-cd/nixos/remote-update.ps1
@@ -3,7 +3,7 @@ $SOURCE_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # Set the destination IP and user to use with ssh and scp
 $DEST_USER = "root"
-$DEST_IP = "188.245.145.180"
+$DEST_IP = "188.34.162.159"
 
 # Set the destination directory
 $DEST_DIR = "/etc/nixos"
