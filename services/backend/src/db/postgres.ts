7e9ae821ea3551a14064a5f28efc7c48e3847f04
Hans Askov <hansaskov1999@gmail.com>
2024-11-27 10:30:55 +0100

    Update backend to exit if there is no connection

--- services/backend/src/db/postgres.ts
+++ services/backend/src/db/postgres.ts
@@ -1,4 +1,25 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { environment } from "../environment";
+import { exit } from "process";
+import { systems } from "./tables";
 
 export const db = drizzle(environment.DATABASE_URL, { casing: "snake_case" });
+
+const testDbConnection = async () => {
+    try {
+        console.log("INFO: Testing db connection to systems table");
+
+        // Await the database query
+        await db.select({ id: systems.id }).from(systems).limit(1);
+
+        console.log("INFO: Testing succeeded, continuing");
+
+    } catch (e) {
+        console.error(e)
+        console.error("CRITICAL ERROR: Cannot access systems table. Shutting down");
+        exit(1)
+    }
+};
+
+// Invoke the testDbConnection function on startup
+testDbConnection();
