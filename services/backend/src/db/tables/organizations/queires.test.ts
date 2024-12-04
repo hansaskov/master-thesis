9991d3b3db1993476ba50531eca5c6004a250195
Hans Askov <hansaskov1999@gmail.com>
2024-12-04 08:08:23 +0100

    Create unit tests for queries organization on users

---
+++ services/backend/src/db/tables/organizations/queires.test.ts
@@ -0,0 +1,81 @@
+import { beforeAll, describe, expect, it } from "bun:test";
+import { treaty } from "@elysiajs/eden";
+import { Elysia } from "elysia";
+import { Queries, Table } from "../../model";
+import { db } from "../../postgres";
+
+
+describe("Organization Queries", async () => {
+
+	it("Select 1 organization on users", async () => {
+
+		const organizations = await Queries.organizations.create({
+			name: "Trivision_test",
+		});
+
+		const user = await Queries.users.create({
+			provider_id: "kgæajvpajnbir",
+			provider_name: "Microsoft",
+			is_superadmin: true,
+		});
+
+		const relationship = await Queries.usersToOrganizations.create({
+			organization_id: organizations.id,
+			user_id: user.id,
+			role: "Admin"
+		})
+
+		const queried_organizations =
+			await Queries.organizations.selectOrganizationsOnUser({
+				id: user.id,
+			})
+
+		expect(queried_organizations[0].id).toBe(organizations.id);
+		expect(queried_organizations[0].userRole).toBe(relationship.role);
+	});
+
+	it("Select multiple organizations on users", async () => {
+
+		const organizations1 = await Queries.organizations.create({
+			name: "Trivision_test",
+		});
+
+		const organizations2 = await Queries.organizations.create({
+			name: "Thiese_ost_test",
+		});
+
+		const user = await Queries.users.create({
+			provider_id: "kgæajvpajnbir",
+			provider_name: "Microsoft",
+			is_superadmin: true,
+		});
+
+		const relationship1 = await Queries.usersToOrganizations.create({
+			organization_id: organizations1.id,
+			user_id: user.id,
+			role: "Admin"
+		})
+
+		const relationship2 = await Queries.usersToOrganizations.create({
+			organization_id: organizations2.id,
+			user_id: user.id,
+			role: "User"
+		})
+
+		const queried_organizations =
+		await Queries.organizations.selectOrganizationsOnUser({
+				id: user.id,
+			})
+
+		expect(queried_organizations).toContainEqual({
+			id: organizations1.id,
+			name: organizations1.name,
+			userRole: relationship1.role
+		});
+		expect(queried_organizations).toContainEqual({
+			id: organizations2.id,
+			name: organizations2.name,
+			userRole: relationship2.role
+		});
+	});
+});
