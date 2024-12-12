832803aba0cbc64b3cbc50c731788fed662a89fb
hansaskov <hansaskov1999@gmail.com>
2024-12-13 00:38:51 +0100

    Add error handling for schema

--- services/frontend/src/lib/error.ts
+++ services/frontend/src/lib/error.ts
@@ -2,12 +2,14 @@ import { toast } from 'svelte-sonner';
 
 export function onError(error: string | { value: string } | { value: unknown } | null) {
 	if (error) {
-		console.error(error);
+		console.log(JSON.stringify(error, null, 4));
 
 		if (typeof error === 'string') {
 			toast.error(error);
 		} else if ('value' in error && typeof error.value === 'string') {
 			toast.error(error.value);
+		} else if ('value' in error && typeof error.value === 'object' &&  error.value && 'message' in error.value && typeof error.value.message ===  "string") {
+			toast.error(error.value.message);
 		} else {
 			toast.error('Unknown error occured');
 		}
