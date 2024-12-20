f765e1d6ab85bf0ddb18dee6cfe225ccb16421e7
Hans Askov <hans@askov.dk>
2024-12-21 00:48:30 +0100

    Start update to shadcn-svelte 5

--- services/frontend/tailwind.config.ts
+++ services/frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config: Config = {
 	darkMode: ['class'],
@@ -68,9 +69,29 @@ const config: Config = {
 			fontFamily: {
 				sans: ['font-geist-sans', 'sans-serif'],
 				mono: ['font-geist-mono', 'monospace']
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--bits-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--bits-accordion-content-height)' },
+					to: { height: '0' }
+				},
+				'caret-blink': {
+					'0%,70%,100%': { opacity: '1' },
+					'20%,50%': { opacity: '0' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'caret-blink': 'caret-blink 1.25s ease-out infinite'
 			}
 		}
-	}
+	},
+	plugins: [tailwindcssAnimate]
 };
 
 export default config;
