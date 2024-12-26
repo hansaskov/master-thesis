3c8a41d78199bb04b8d94061a2af8e418ca99c1c
hansaskov <hansaskov1999@gmail.com>
2024-12-26 17:16:49 +0100

    Update toast to be from the top for mobile users

--- services/frontend/src/routes/+layout.svelte
+++ services/frontend/src/routes/+layout.svelte
@@ -2,13 +2,15 @@
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
+	import { innerWidth } from 'svelte/reactivity/window';
 	import ResponsiveDialog from '$lib/components/ResponsiveDialog.svelte';
+	let isDesktop = $derived(innerWidth.current! > 768);
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher defaultTheme="light" />
-<Toaster />
+<Toaster position={isDesktop ? "bottom-right" : "top-center"}/>
 <ResponsiveDialog />
 
 {@render children()}
