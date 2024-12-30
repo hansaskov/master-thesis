db232cced16b1526951e66092ca664145c0657ef
hansaskov <hansaskov1999@gmail.com>
2024-12-30 17:36:15 +0100

    Simplify organization page

--- services/frontend/src/routes/(app)/organization/+page.svelte
+++ services/frontend/src/routes/(app)/organization/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
-
 	import { goto } from '$app/navigation';
 	import { organizationStore } from '$lib/stores/organization.svelte';
 
+	// Fetch organizations on load
 	organizationStore.refresh();
 
-	$effect.pre(() => {
-		if (organizationStore.currentOrganization) {
-			goto(`/organization/${organizationStore.currentOrganization.id}/systems`);
-		} else if (organizationStore.organizations.length > 0) {
-			goto(`/organization/${organizationStore.organizations[0].id}/systems`);
+	$effect(() => {
+		// Redirect only if orgs exist
+		const target =
+			organizationStore.currentOrganization?.id || organizationStore.organizations[0]?.id;
+		if (target) {
+			goto(`/organization/${target}/systems`);
 		}
 	});
 </script>
