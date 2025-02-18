85112a34e398e9ac07c1a3f85685c50ef006cc85
Hans Askov <ha@trivision.dk>
2025-02-18 09:31:16 +0100

    Add resend as a email client

--- services/backend/src/email/interface.ts
+++ services/backend/src/email/interface.ts
@@ -1,1 +1,9 @@
-console.log("Hello");
+// First, define an interface for our email service
+export interface IEmailSecrvice {
+	send(
+		from: string,
+		to: string,
+		subject: string,
+		html: string,
+	): Promise<boolean>;
+}
