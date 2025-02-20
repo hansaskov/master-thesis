57492a59d137b923c51015196312242513903b25
Hans Askov <70580312+hansaskov@users.noreply.github.com>
2025-02-20 20:25:50 +0100

    Org settings update (#98)
    * Update to my own branch
    
    * Finish up the creationn of keys on systems
    
    * Fix issue in prod where values where systemid was not correctly set
    
    ---------
    
    Co-authored-by: Hans Askov <ha@trivision.dk>
    Co-authored-by: Hans Askov <hans@askov.dk>

--- services/frontend/src/routes/(app)/organization/[organizationId]/system/[systemId]/settings/+page.svelte
+++ services/frontend/src/routes/(app)/organization/[organizationId]/system/[systemId]/settings/+page.svelte
@@ -3,26 +3,31 @@
 	import KeysForm from './KeysForm.svelte';
 	import KeysList from './KeysList.svelte';
 	import { dialogStore } from '@/stores/dialog.svelte';
+	import { userStore } from '@/stores/user.svelte';
 </script>
 
 <div class="container mx-auto px-4 py-4">
 	<div class="flex flex-row justify-between">
 		<h1 class="mb-6 text-3xl font-bold">Settings</h1>
-		<Button
-			onclick={() => {
-				dialogStore.open({
-					title: `Create a new Api Key`,
-					description: 'This action will create a new api key on your system',
-					component: KeysForm,
-					props: {}
-				});
-			}}
-		>
-			+ Create API Key
-		</Button>
+		{#if userStore.isAdmin}
+			<Button
+				onclick={() => {
+					dialogStore.open({
+						title: `Create a new Api Key`,
+						description: 'This action will create a new api key on your system',
+						component: KeysForm,
+						props: {}
+					});
+				}}
+			>
+				+ Create API Key
+			</Button>
+		{/if}
 	</div>
 
 	<div class="grid grid-cols-1 gap-6 md:grid-cols-2">
-		<KeysList />
+		{#if userStore.isAdmin}
+			<KeysList />
+		{/if}
 	</div>
 </div>
