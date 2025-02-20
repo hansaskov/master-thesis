545e00c02d91dc549ebe1ae1a855f5bfe9846754
Sebastian <sechr17@student.sdu.dk>
2025-02-20 13:03:12 +0100

    migration attempt X

--- services/backend/src/collections/system_models/queries.test.ts
+++ services/backend/src/collections/system_models/queries.test.ts
@@ -4,30 +4,24 @@ import { generateRandomString } from "$utils/random";
 
 describe("system model queries", async () => {
 	it("select all parts for system model", async () => {
-		const system_model = await Queries.systemModels.selectOnId({
-			id: "Dgw8Rvd4QoeC",
+		const all_models_with_all_parts = await Queries.systemModels.selectAll();
+		expect(all_models_with_all_parts.length).toEqual(7);
+
+		const testSystemModel = await Queries.systemModels.create({
+			name: "VisioPointer",
 		});
 
 		const part = await Queries.part.create({ name: "Frozen Pizza" });
 		const part2 = await Queries.part.create({ name: "Some Camera" });
 
-		const relationship1 = await Queries.partsToSystemModels.create({
-			part_id: part.id,
-			system_model_id: system_model[0].id,
-		});
-		const relationship2 = await Queries.partsToSystemModels.create({
-			part_id: part2.id,
-			system_model_id: system_model[0].id,
-		});
-
-		const all_models_with_all_parts = await Queries.systemModels.selectAll();
-		console.log(all_models_with_all_parts);
 		const firstSystemModel = all_models_with_all_parts[0];
 
-		expect(system_model.length).toEqual(1);
-		expect(all_models_with_all_parts.length).toEqual(7);
 		expect(firstSystemModel.id).toBeDefined();
 		expect(firstSystemModel.name).toBeDefined();
 		expect(firstSystemModel.parts).toBeArray();
+
+		await Queries.systemModels.delete(testSystemModel);
+		await Queries.part.delete(part);
+		await Queries.part.delete(part2);
 	});
 });
--- services/backend/src/db/migrations/0002_abnormal_slayback.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "invites" DROP CONSTRAINT "invites_email_organization_id_pk";--> statement-breakpoint
-ALTER TABLE "invites" ADD COLUMN "id" text PRIMARY KEY NOT NULL;--> statement-breakpoint
-ALTER TABLE "invites" ADD COLUMN "is_accepted" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "invites" ADD COLUMN "expires_at" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "parts" ADD COLUMN "image" text;--> statement-breakpoint
-CREATE INDEX "invites_email_organization_id_index" ON "invites" USING btree ("email","organization_id");--> statement-breakpoint
-ALTER TABLE "system_models" ADD CONSTRAINT "system_models_name_unique" UNIQUE("name");
