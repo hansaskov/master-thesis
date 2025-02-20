6c20ca95bc7f3ae30834dcba491d4487223c98c3
Hans Askov <ha@trivision.dk>
2025-02-20 13:29:23 +0100

    Use correct migrate

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -2,24 +2,29 @@ import cluster from "node:cluster";
 import { availableParallelism } from "node:os";
 import process from "node:process";
 import { app } from "./server";
+import { env } from "elysia";
 
 export type App = typeof app;
 export type * as Types from "$collections/types";
 
-if (cluster.isPrimary) {
-	console.log(`Primary ${process.pid} is running`);
+const signals = ["SIGINT", "SIGTERM"]
 
-	// Start N workers for the number of CPUs
-	const numberOfThreads = 1;
-	for (let i = 0; i < numberOfThreads; i++) {
-		cluster.fork();
-	}
-
-	cluster.on("exit", (worker) => {
-		console.log(`worker ${worker.process.pid} died`);
-		process.exit(1);
-	});
-} else {
-	app.listen(process.env.PORT as string);
-	console.log(`🦊 Worker ${process.pid} started at ${app.server?.url.origin}`);
+for (const signal of signals) {
+	process.on(signal, async () => {
+		console.log(`Received ${signal}. Initiating graceful shutdown...`)
+		await app.stop()
+		process.exit(0)
+	})
 }
+
+process.on("uncaughtException", error => {
+	console.error(error)
+})
+
+process.on("unhandledRejection", error => {
+	console.error(error)
+})
+
+app.listen(env.PORT ?? 3000, () =>
+	console.info("🚀 Server started at http://127.0.0.1:3000/")
+)
