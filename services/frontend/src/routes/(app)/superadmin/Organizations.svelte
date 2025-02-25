dcca10f7785b09c0a51b9ac6c05394d02d002b3f
Sebastian689 <79843703+Sebastian689@users.noreply.github.com>
2025-02-25 09:30:47 +0100

    Superadmin overview into preview (#99)
    * so far
    
    * so far
    
    * pagination bug
    
    * format and lint

--- services/frontend/src/routes/(app)/superadmin/Organizations.svelte
+++ services/frontend/src/routes/(app)/superadmin/Organizations.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
 	import * as Table from '$lib/components/ui/table';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { organizationStore } from '$lib/stores/organization.svelte';
 	import type { Types } from 'backend';
-	import { partsStore } from '$lib/stores/parts.svelte';
 	import { Ellipsis, ChevronLeft, ChevronRight } from 'lucide-svelte';
 	import { dialogStore } from '$lib/stores/dialog.svelte';
+	import EditOrganizationDialogBody from '$lib/components/EditOrganizationDialogBody.svelte';
 	import AlertDialogBody from '$lib/components/AlertDialogBody.svelte';
-	import EditPartDialogBody from '$lib/components/EditPartDialogBody.svelte';
 
-	partsStore.refresh();
+	organizationStore.refresh();
 
-	// parts pagination
 	let pageSize = 10;
 	let currentPage = $state(0);
 	let startIndex = $derived(currentPage * pageSize);
 	let endIndex = $derived((currentPage + 1) * pageSize);
-	let visibleParts = $derived(partsStore.parts.slice(startIndex, endIndex));
-	let totalItems = $derived(partsStore.parts.length);
+	let visibleOrganizations = $derived(organizationStore.organizations.slice(startIndex, endIndex));
+	let totalOrganizations = $derived(organizationStore.organizations.length);
+	let totalItems = organizationStore.organizations.length;
 	let totalPages = $derived(Math.ceil(totalItems / pageSize));
 
 	function prevPage() {
@@ -35,29 +35,28 @@
 		}
 	}
 
-	let newPart = $state<Types.PartNew>({
+	let newOrganization = $state<Types.OrganizationNew>({
 		name: ''
 	});
-
-	function add(e: SubmitEvent) {
-		e.preventDefault();
-		partsStore.add(newPart);
-		newPart = {
-			name: ''
-		};
-	}
 </script>
 
 <Card.Root class="col-span-1 md:col-span-2">
 	<Card.Header>
-		<Card.Title>Vision System Spare Parts</Card.Title>
+		<Card.Title>Organization Management</Card.Title>
 	</Card.Header>
 	<Card.Content>
 		<div class="mb-6">
-			<Label for="new-part">Add New Spare Part</Label>
-			<form class="flex gap-2" onsubmit={add}>
-				<Input placeholder="Enter spare part name" bind:value={newPart.name} />
-				<Button type="submit">Add Part</Button>
+			<Label for="new-organization">Add New Organization</Label>
+			<form
+				onsubmit={(e) => {
+					e.preventDefault();
+					organizationStore.add(newOrganization);
+					newOrganization.name = '';
+				}}
+				class="flex gap-2"
+			>
+				<Input placeholder="Enter organization name" bind:value={newOrganization.name} />
+				<Button type="submit">Add Organization</Button>
 			</form>
 		</div>
 
@@ -66,8 +65,8 @@
 				<Button variant="outline" onclick={() => prevPage()} disabled={currentPage === 0}>
 					<ChevronLeft class="w-2 h-2" />
 				</Button>
-				Showing {startIndex + 1}–{startIndex + visibleParts.length}
-				of {totalItems} results
+				Showing {startIndex + 1}–{startIndex + visibleOrganizations.length}
+				of {totalOrganizations} results
 				<Button
 					variant="outline"
 					onclick={() => nextPage()}
@@ -78,16 +77,14 @@
 			</Table.Caption>
 			<Table.Header>
 				<Table.Row>
-					<Table.Head>Image</Table.Head>
 					<Table.Head>Name</Table.Head>
 					<Table.Head class="text-right">Actions</Table.Head>
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{#each visibleParts as part}
+				{#each visibleOrganizations as organization}
 					<Table.Row>
-						<Table.Cell>{part.image}</Table.Cell>
-						<Table.Cell>{part.name}</Table.Cell>
+						<Table.Cell>{organization.name}</Table.Cell>
 						<Table.Cell class="text-right">
 							<DropdownMenu.Root>
 								<DropdownMenu.Trigger>
@@ -102,28 +99,30 @@
 									<DropdownMenu.Item
 										onclick={() => {
 											dialogStore.open({
-												title: `Update ${part.name}`,
-												description: 'This action will update the name of the selected part',
-												component: EditPartDialogBody,
-												props: part
+												title: `Update ${organization.name}`,
+												description:
+													'This action will update the name of the selected organization',
+												component: EditOrganizationDialogBody,
+												props: organization
 											});
 										}}
 									>
-										Edit Part
+										Edit Organization
 									</DropdownMenu.Item>
 									<DropdownMenu.Separator />
 									<DropdownMenu.Item
 										onclick={() => {
 											dialogStore.open({
 												title: 'Are you absolutely sure?',
-												description: 'This action cannot be undone',
+												description:
+													"This action cannot be undone. This will permanently delete the organization and all of it's systems",
 												component: AlertDialogBody,
-												props: { onsubmit: () => partsStore.remove(part) }
+												props: { onsubmit: () => organizationStore.remove(organization) }
 											});
 										}}
 										class="text-red-600"
 									>
-										Remove Part
+										Remove Organization
 									</DropdownMenu.Item>
 								</DropdownMenu.Content>
 							</DropdownMenu.Root>
---
+++ services/frontend/src/routes/(app)/superadmin/Users.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import * as Table from '$lib/components/ui/table';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import { ChevronDown, ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { userStore } from '$lib/stores/user.svelte';
+
+	userStore.loadAllUser();
+
+	let searchTerm = $state('');
+	let filteredUsers = $derived(
+		userStore.allUsers.current.filter(
+			(user) =>
+				user.name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				user.email?.toLowerCase().includes(searchTerm.toLowerCase())
+		)
+	);
+
+	let pageSize = 10;
+	let currentPage = $state(0);
+	let totalItems = $derived(filteredUsers.length);
+	let totalPages = $derived(Math.ceil(totalItems / pageSize));
+
+	let startIndex = $derived(currentPage * pageSize);
+	let endIndex = $derived((currentPage + 1) * pageSize);
+	let visibleUsers = $derived(filteredUsers.slice(startIndex, endIndex));
+
+	function prevPage() {
+		if (currentPage > 0) {
+			currentPage--;
+		}
+	}
+
+	function nextPage() {
+		if (currentPage < totalPages) {
+			currentPage++;
+		}
+	}
+
+	async function handleRoleChange(id: string, newValue: boolean) {
+		await userStore.edit(id, newValue);
+	}
+</script>
+
+<Card.Root class="col-span-1 md:col-span-2">
+	<Card.Header>
+		<Card.Title>User Management</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<div class="space-y-2">
+			<Label>Search Users</Label>
+			<Input
+				bind:value={searchTerm}
+				placeholder="Search by name or email..."
+				class="max-w-[400px]"
+			/>
+		</div>
+
+		<Table.Root class="table-fixed">
+			<Table.Caption>
+				<Button variant="outline" onclick={() => prevPage()} disabled={currentPage === 0}>
+					<ChevronLeft class="w-2 h-2" />
+				</Button>
+				Showing {startIndex + 1}–{startIndex + visibleUsers.length}
+				of {totalItems} results
+				<Button
+					variant="outline"
+					onclick={() => nextPage()}
+					disabled={currentPage + 1 === totalPages}
+				>
+					<ChevronRight class="w-2 h-2" />
+				</Button>
+			</Table.Caption>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="text-left w-28">Name</Table.Head>
+					<Table.Head class="hidden md:table-cell text-left">Email</Table.Head>
+					<Table.Head class="text-right">Role</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			{#each visibleUsers as user}
+				<Table.Row>
+					<Table.Cell>
+						{user.name}
+					</Table.Cell>
+					<Table.Cell class="hidden md:table-cell">
+						{user.email}
+					</Table.Cell>
+					<Table.Cell class="text-right">
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger class="">
+								<Button variant="ghost" class="">
+									<span>{user.is_superadmin ? 'Superadmin' : 'User'}</span>
+									<ChevronDown class="h-4 w-4" />
+								</Button>
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Content align="end">
+								<DropdownMenu.Label>Change Role</DropdownMenu.Label>
+								<DropdownMenu.Separator />
+								<DropdownMenu.Item onclick={() => handleRoleChange(user.id, true)}>
+									Superadmin
+								</DropdownMenu.Item>
+								<DropdownMenu.Item onclick={() => handleRoleChange(user.id, false)}>
+									User
+								</DropdownMenu.Item>
+							</DropdownMenu.Content>
+						</DropdownMenu.Root>
+					</Table.Cell>
+				</Table.Row>
+			{:else}
+				<Table.Row>
+					<Table.Cell colspan={3} class="text-center">
+						{searchTerm ? `No users found matching "${searchTerm}"` : 'No users found'}
+					</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Root>
+	</Card.Content>
+</Card.Root>
