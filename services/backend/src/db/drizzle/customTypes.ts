17668113702b6365d3cdddec13b46064f257b16a
Hans Askov <ha@trivision.dk>
2025-03-18 15:29:01 +0100

    linting

--- services/backend/src/db/drizzle/customTypes.ts
+++ services/backend/src/db/drizzle/customTypes.ts
@@ -1,30 +1,55 @@
-import { AnyColumn, Column, ColumnBaseConfig, is, sql, type SQL, type SQLWrapper } from 'drizzle-orm';
-import { AnyPgColumn, customType, PgColumn, PgTimestamp } from 'drizzle-orm/pg-core';
+import {
+	type AnyColumn,
+	Column,
+	type SQL,
+	type SQLWrapper,
+	is,
+	sql,
+} from "drizzle-orm";
 
-type ValidUnits = 'microseconds' | 'milliseconds' | 'second' | 'seconds' | 'minute' | 'minutes' | 'hour' | 'hours' | 'day' | 'days' | 'week' | 'weeks' | 'month' | 'months' | 'year' | 'years';
+type ValidUnits =
+	| "microseconds"
+	| "milliseconds"
+	| "second"
+	| "seconds"
+	| "minute"
+	| "minutes"
+	| "hour"
+	| "hours"
+	| "day"
+	| "days"
+	| "week"
+	| "weeks"
+	| "month"
+	| "months"
+	| "year"
+	| "years";
 
 type IntervalString = `${number} ${ValidUnits}`;
 
 export function TsTimeBucket(
-    interval: IntervalString,
-    expression: SQLWrapper,
+	interval: IntervalString,
+	expression: SQLWrapper,
 ): SQL<Date> {
-    return sql`time_bucket(${interval}, ${expression})`.mapWith((v) => new Date(v));
+	return sql`time_bucket(${interval}, ${expression})`.mapWith(
+		(v) => new Date(v),
+	);
 }
-
-
 
 export function avg(expression: SQLWrapper): SQL<string> {
 	return sql`avg(${expression})`.mapWith(String);
 }
 
-
-export function first<T extends SQLWrapper>(valueExpr: T, groupByExpr?: T ): SQL<(T extends AnyColumn ? T['_']['data'] : string)> {
-    return sql`first(${valueExpr}, ${groupByExpr ?? valueExpr})`.mapWith(is(valueExpr, Column) ? valueExpr : String) as any
+export function first<T extends SQLWrapper>(valueExpr: T, groupByExpr?: T) {
+	return sql`first(${valueExpr}, ${groupByExpr ?? valueExpr})`.mapWith(
+		is(valueExpr, Column) ? valueExpr : String,
+	);
 }
 
-export function last<T extends SQLWrapper>(valueExpr: T, groupByExpr?: T ): SQL<(T extends AnyColumn ? T['_']['data'] : string)> {
-    return sql`last(${valueExpr}, ${groupByExpr ?? valueExpr})`.mapWith(is(valueExpr, Column) ? valueExpr : String) as any
+export function last<T extends SQLWrapper>(valueExpr: T, groupByExpr?: T) {
+	return sql`last(${valueExpr}, ${groupByExpr ?? valueExpr})`.mapWith(
+		is(valueExpr, Column) ? valueExpr : String,
+	);
 }
 
 /**
@@ -37,8 +62,10 @@ export function last<T extends SQLWrapper>(valueExpr: T, groupByExpr?: T ): SQL<
  * db.select({ value: max(employees.salary) }).from(employees)
  * ```
  */
-export function max<T extends SQLWrapper>(expression: T): SQL<(T extends AnyColumn ? T['_']['data'] : string)> {
-	return sql`max(${expression})`.mapWith(is(expression, Column) ? expression : String) as any;
+export function max<T extends SQLWrapper>(expression: T) {
+	return sql`max(${expression})`.mapWith(
+		is(expression, Column) ? expression : String,
+	);
 }
 
 /**
@@ -51,7 +78,8 @@ export function max<T extends SQLWrapper>(expression: T): SQL<(T extends AnyColu
  * db.select({ value: min(employees.salary) }).from(employees)
  * ```
  */
-export function min<T extends SQLWrapper>(expression: T): SQL<(T extends AnyColumn ? T['_']['data'] : string)> {
-	return sql`min(${expression})`.mapWith(is(expression, Column) ? expression : String) as any;
+export function min<T extends SQLWrapper>(expression: T) {
+	return sql`min(${expression})`.mapWith(
+		is(expression, Column) ? expression : String,
+	);
 }
-
