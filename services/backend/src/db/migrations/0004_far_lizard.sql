fbaadacd4763e97f2f4ad6f7f130b2b6daccd067
Hans Askov <ha@trivision.dk>
2025-03-18 09:32:54 +0100

    reset migrations

--- services/backend/src/db/migrations/0004_far_lizard.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "public"."users" ALTER COLUMN "provider_name" SET DATA TYPE text;--> statement-breakpoint
-DROP TYPE "public"."providers";--> statement-breakpoint
-CREATE TYPE "public"."providers" AS ENUM('Github', 'Microsoft');--> statement-breakpoint
-ALTER TABLE "public"."users" ALTER COLUMN "provider_name" SET DATA TYPE "public"."providers" USING "provider_name"::"public"."providers";
