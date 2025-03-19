9e6735d66c06694da6b871cbccbb17770d9da9c7
Hans Askov <ha@trivision.dk>
2025-03-19 16:34:42 +0100

    refactor into it's own component

--- services/frontend/src/routes/(app)/organization/[organizationId]/system/[systemId]/monitoring/+page.svelte
+++ services/frontend/src/routes/(app)/organization/[organizationId]/system/[systemId]/monitoring/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import * as Card from '$lib/components/ui/card';
-	import { Area, AreaChart, LinearGradient, Tooltip } from 'layerchart';
-	import TimeRangeSelector from '../TimeRangeSelector.svelte';
 
 	import { timeRangeStore } from '../TimeRangeStore.svelte';
-	import { DateFormatter, getLocalTimeZone } from '@internationalized/date';
+	import { getLocalTimeZone } from '@internationalized/date';
 	import Metrics from './Metrics.svelte';
-	import { groupBy } from '@/utils';
 	import { monitorStore } from '@/stores/monitor.svelte';
-
-	let chartsData = $derived.by(() => {
-		return Array.from(groupBy(monitorStore.readings, 'name'), ([name, readings]) => ({
-			name,
-			data: readings.map(({ time, value }) => ({
-				date: new Date(time),
-				value
-			}))
-		}));
-	});
+	import MonitoringCharts from './MonitoringCharts.svelte';
 
 	async function fetchReadings() {
 		const { start, end } = timeRangeStore.range;
@@ -51,45 +38,9 @@
 	onMount(() => {
 		fetchReadings();
 	});
-
-	const df = new DateFormatter('en-US', { timeStyle: 'medium' });
 </script>
 
 <!-- Charts Section -->
-
-<div class="mb-8">
-	<div class="mb-4 flex flex-col md:flex-row justify-between md:items-center">
-		<h2 class="text-2xl font-bold">Charts</h2>
-
-		<TimeRangeSelector></TimeRangeSelector>
-	</div>
-
-	<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-		{#each chartsData as { data, name }}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title class="text-xl font-bold">{name}</Card.Title>
-				</Card.Header>
-				<Card.Content class="h-[300px] p-4 border rounded">
-					<AreaChart {data} x="date" y="value">
-						<svelte:fragment slot="marks">
-							<LinearGradient class="from-primary/50 to-primary/0" vertical let:gradient>
-								<Area line={{ class: 'stroke-primary' }} fill={gradient} />
-							</LinearGradient>l
-						</svelte:fragment>
-						<svelte:fragment slot="tooltip">
-							<Tooltip.Root let:data>
-								<Tooltip.Header>{df.format(data.date)}</Tooltip.Header>
-								<Tooltip.List>
-									<Tooltip.Item label="value" value={data.value} />
-								</Tooltip.List>
-							</Tooltip.Root>
-						</svelte:fragment>
-					</AreaChart>
-				</Card.Content>
-			</Card.Root>
-		{/each}
-	</div>
-</div>
+<MonitoringCharts />
 <!-- Metrics Section -->
 <Metrics />
---
+++ services/frontend/src/routes/(app)/organization/[organizationId]/system/[systemId]/monitoring/MonitoringCharts.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import { Area, AreaChart, LinearGradient, Tooltip } from 'layerchart';
+	import TimeRangeSelector from '../TimeRangeSelector.svelte';
+	import { DateFormatter } from '@internationalized/date';
+	import { groupBy } from '@/utils';
+	import { monitorStore } from '@/stores/monitor.svelte';
+
+	let chartsData = $derived.by(() => {
+		return Array.from(groupBy(monitorStore.readings, 'name'), ([name, readings]) => ({
+			name,
+			data: readings.map(({ time, value }) => ({
+				date: new Date(time),
+				value
+			}))
+		}));
+	});
+
+	const df = new DateFormatter('en-US', { timeStyle: 'medium' });
+</script>
+
+<!-- Charts Section -->
+
+<div class="mb-8">
+	<div class="mb-4 flex flex-col md:flex-row justify-between md:items-center">
+		<h2 class="text-2xl font-bold">Charts</h2>
+
+		<TimeRangeSelector></TimeRangeSelector>
+	</div>
+
+	<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+		{#each chartsData as { data, name }}
+			<Card.Root>
+				<Card.Header>
+					<Card.Title class="text-xl font-bold">{name}</Card.Title>
+				</Card.Header>
+				<Card.Content class="h-[300px] p-4 border rounded">
+					<AreaChart {data} x="date" y="value">
+						<svelte:fragment slot="marks">
+							<LinearGradient class="from-primary/50 to-primary/0" vertical let:gradient>
+								<Area line={{ class: 'stroke-primary' }} fill={gradient} />
+							</LinearGradient>l
+						</svelte:fragment>
+						<svelte:fragment slot="tooltip">
+							<Tooltip.Root let:data>
+								<Tooltip.Header>{df.format(data.date)}</Tooltip.Header>
+								<Tooltip.List>
+									<Tooltip.Item label="value" value={data.value} />
+								</Tooltip.List>
+							</Tooltip.Root>
+						</svelte:fragment>
+					</AreaChart>
+				</Card.Content>
+			</Card.Root>
+		{/each}
+	</div>
+</div>
