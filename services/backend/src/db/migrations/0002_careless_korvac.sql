722bbae4ccd72ace5fd790e65e382766edd8d90b
Hans Askov <ha@trivision.dk>
2025-03-19 09:29:55 +0100

    Handle cases where category is null

--- services/backend/src/db/migrations/0002_careless_korvac.sql
+++ services/backend/src/db/migrations/0002_careless_korvac.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "readings" DROP CONSTRAINT "readings_system_id_name_time_pk";--> statement-breakpoint
+UPDATE "readings" SET "category" = 'Others' WHERE "category" IS NULL;--> statement-breakpoint
 ALTER TABLE "readings" ALTER COLUMN "category" SET DEFAULT 'Others';--> statement-breakpoint
 ALTER TABLE "readings" ALTER COLUMN "category" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "readings" ADD CONSTRAINT "readings_system_id_category_unit_name_time_pk" PRIMARY KEY("system_id","category","unit","name","time");
