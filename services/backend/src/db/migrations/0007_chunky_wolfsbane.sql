6c7381b0231a84fc95a50ff32d68eea46e7d56fe
Hans Askov <ha@trivision.dk>
2025-03-20 13:00:27 +0100

    Update migration to set first name to empty string

--- services/backend/src/db/migrations/0007_chunky_wolfsbane.sql
+++ services/backend/src/db/migrations/0007_chunky_wolfsbane.sql
@@ -1,2 +1,4 @@
 ALTER TABLE "users" RENAME COLUMN "name" TO "family_name";--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "first_name" text NOT NULL;
+ALTER TABLE "users" ADD COLUMN "first_name" text;--> statement-breakpoint
+UPDATE "users" SET "first_name" = '';--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "first_name" SET NOT NULL;
