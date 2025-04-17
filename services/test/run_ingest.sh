a689a80e2887bb43fbd940a22613f3f2b5b71945
Hans Askov <hansaskov1999@gmail.com>
2025-04-17 11:25:11 +0200

    Update stress_test again

--- services/test/run_ingest.sh
+++ services/test/run_ingest.sh
@@ -3,4 +3,4 @@
 # Download the binary and make it executable
 wget -q https://github.com/hansaskov/master-thesis/releases/download/1.1.0/ingest -O ingest && \
 chmod +x ingest && \
-./ingest -h
+./ingest &
